e5ceb8407ec74e1e3525f33eff76736c98eb91b6
McCoy Patino <39780829+mccoyp@users.noreply.github.com>
2018-06-19 15:29:31 -0700

    Initial file addition (#2)

---
+++ src/Models/CPU_Usage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PerfMonitor
+{
+    public class CPU_Usage  // contains the percentage of total CPU usage and DateTime of instant
+    {
+        public double usage { get; set; }
+        public DateTime timestamp { get; set; }
+    }
+}
