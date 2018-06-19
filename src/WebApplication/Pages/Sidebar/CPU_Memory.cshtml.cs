5ca5bf90224881c82576b20e602ae63980c8403d
mogarg18 <39780842+mogarg18@users.noreply.github.com>
2018-06-19 15:29:12 -0700

    initial commit (#1)

---
+++ src/WebApplication/Pages/Sidebar/CPU_Memory.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication.Pages.Metrics
+{
+    public class CPU_MemoryModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ src/WebApplication/Pages/Sidebar/Garbage_Collection.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication.Pages.Metrics
+{
+    public class Garbage_CollectionModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
