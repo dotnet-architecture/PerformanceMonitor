41c66f1ddeee14aaf89caaa883108b1b4b62bd44
Jacob Kirsch <t-jakirs@microsoft.com>
2018-06-20 11:34:05 -0700

    updated startup.cs and Class.cs

--- src/DataTransfer/Context/Class.cs
+++ src/DataTransfer/Context/Class.cs
@@ -1,8 +1,10 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.EntityFrameworkCore;
 using PerfMonitor;
 
 
 public class CPUContext : DbContext
 {
+    public CPUContext(DbContextOptions<CPUContext> options) :base(options)
+    { }
     public DbSet<CPU_Usage> CPU_Data { get; set; }
 }
