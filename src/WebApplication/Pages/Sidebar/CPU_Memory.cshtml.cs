0320573d57f8ac7c2b41f8bb5feef9c0431a2044
Monal Garg <t-mogarg@microsoft.com>
2018-06-21 14:38:38 -0700

    routing to API to get CPU data

--- src/WebApplication/Pages/Sidebar/CPU_Memory.cshtml.cs
+++ src/WebApplication/Pages/Sidebar/CPU_Memory.cshtml.cs
@@ -11,12 +11,13 @@ namespace WebApplication.Pages.Metrics
 {
     public class CPU_MemoryModel : PageModel
     {
-
         CPU_Usage cpu = new CPU_Usage();
         Mem_Usage mem = new Mem_Usage();
         public async void OnGet()
         {
             HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://localhost:44334/");
 
             //HttpResponseMessage response = await client.GetAsync("INSERT API ADDRESS HERE"); 
 
