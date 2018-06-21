6bcfd96a514b890667212a33a4847245d68a52dd
Monal Garg <t-mogarg@microsoft.com>
2018-06-21 14:33:01 -0700

    setting up initial HTTPClient

--- src/WebApplication/Pages/Sidebar/CPU_Memory.cshtml.cs
+++ src/WebApplication/Pages/Sidebar/CPU_Memory.cshtml.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PerfMonitor;
 
 namespace WebApplication.Pages.Metrics
 {
     public class CPU_MemoryModel : PageModel
     {
-        //HttpClient httpClient = new HttpClient();
-        public void OnGet()
+
+        CPU_Usage cpu = new CPU_Usage();
+        Mem_Usage mem = new Mem_Usage();
+        public async void OnGet()
         {
+            HttpClient client = new HttpClient();
+
+            //HttpResponseMessage response = await client.GetAsync("INSERT API ADDRESS HERE"); 
+
+            // Deserialize JSON object from response and update cpu and mem
 
         }
     }
