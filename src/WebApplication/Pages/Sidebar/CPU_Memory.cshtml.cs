9b12709f6c6ade546e387960d03f0c34ef02614a
Monal Garg <t-mogarg@microsoft.com>
2018-06-21 17:27:05 -0700

    setting up MetricService and CPU_MemoryModel

--- src/WebApplication/Pages/Sidebar/CPU_Memory.cshtml.cs
+++ src/WebApplication/Pages/Sidebar/CPU_Memory.cshtml.cs
@@ -6,22 +6,35 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using PerfMonitor;
+using WebApplication.Interfaces;
+
 
 namespace WebApplication.Pages.Metrics
 {
     public class CPU_MemoryModel : PageModel
     {
-        CPU_Usage cpu = new CPU_Usage();
-        Mem_Usage mem = new Mem_Usage();
-        public async void OnGet()
+        private readonly IMetricService _metricService; 
+
+        public CPU_MemoryModel(IMetricService metricService)
+        {
+            _metricService = metricService; 
+        }
+        public CPU_Usage cpu { get; set; } = new CPU_Usage();
+
+        public Mem_Usage mem { get; set; } = new Mem_Usage();
+
+        public async Task OnGet()
         {
             HttpClient client = new HttpClient();
 
             client.BaseAddress = new Uri("http://localhost:44334/");
 
-            //HttpResponseMessage response = await client.GetAsync("INSERT API ADDRESS HERE"); 
+            HttpResponseMessage response = await client.GetAsync("api/v1/CPU/CPU");
+            response.EnsureSuccessStatusCode();
 
             // Deserialize JSON object from response and update cpu and mem
+            cpu = await _metricService.getCPUUsage();
+            mem = await _metricService.getMemUsage();
 
         }
     }
---
+++ src/WebApplication/Services/MetricService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using PerfMonitor;
+using WebApplication.Interfaces;
+
+namespace WebApplication.Services
+{
+    public class MetricService : IMetricService
+    {
+
+        CPU_Usage cpu = new CPU_Usage();
+        Mem_Usage mem = new Mem_Usage();
+        public async Task<CPU_Usage> getCPUUsage()
+        {
+            // await Http call
+            return cpu;
+        }
+
+        public async Task<Mem_Usage> getMemUsage()
+        {
+            // await Http call
+            return mem; 
+        }
+
+        public void updateUsingHttpResponse(HttpResponseMessage response)
+        {
+            // Desearilizes response JSON file 
+            // updates CPU_Usage
+            // updates Mem_Usage
+        }
+    }
+}
