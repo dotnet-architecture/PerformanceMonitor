d049c41ccbe372db5343fd01fce546b5a897d1f8
mogarg18 <39780842+mogarg18@users.noreply.github.com>
2018-06-21 17:47:17 -0700

    Adding MetricService and adding it to CPU_MemoryModel (#14)
    * implementing MetricService

---
+++ src/WebApplication/Interfaces/IMetricService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using PerfMonitor;
+
+namespace WebApplication.Interfaces
+{
+    public interface IMetricService
+    {
+        void updateUsingHttpResponse(HttpResponseMessage response);
+        Task<CPU_Usage> getCPUUsage();
+        Task<Mem_Usage> getMemUsage();
+
+    }
+}
