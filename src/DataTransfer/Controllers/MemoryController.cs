33cb8eec010956f311b624b7eb503b0c42f53cb1
Jacob Kirsch <t-jakirs@microsoft.com>
2018-06-26 10:45:56 -0700

    restructured the Context class and added context for a memory table

---
+++ src/DataTransfer/Controllers/MemoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerfMonitor.Controllers
+{
+    public class MemoryController : ControllerBase
+    {
+    }
+}
