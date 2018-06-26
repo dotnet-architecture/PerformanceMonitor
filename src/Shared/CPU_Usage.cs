b2841c20188322509a79fb1aafd9c0d2d2cf7e5f
Jacob Kirsch <t-jakirs@microsoft.com>
2018-06-26 13:46:48 -0700

    added primary keys for the classes

--- src/Shared/CPU_Usage.cs
+++ src/Shared/CPU_Usage.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PerfMonitor
 {
     public class CPU_Usage  // contains the percentage of total CPU usage and DateTime of instant
     {
         public double usage { get; set; }
+        [Key]
         public DateTime timestamp { get; set; }
     }
 }
--- src/Shared/Mem_Usage.cs
+++ src/Shared/Mem_Usage.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PerfMonitor
 {
     public class Mem_Usage  // contains the number of bytes used by process, and timestamp
     {
         public long usage { get; set; }
+        [Key]
         public DateTime timestamp { get; set; }
     }
 }
