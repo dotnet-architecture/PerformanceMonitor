18509930e237a3267157f60d1f4296088b877b9b
McCoy Patino Middaugh <t-mcpati@microsoft.com>
2018-06-29 14:39:10 -0700

    added support for exceptions

--- src/DataTransfer/Startup.cs
+++ src/DataTransfer/Startup.cs
@@ -20,7 +20,7 @@ namespace PerfMonitor
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            var connection = "Server = 10.0.75.1,1433; Initial Catalog = PerformanceData  ; User Id = sa; Password = Abc12345";
+            var connection = "Server = 10.0.75.1,1433; Initial Catalog = PerformanceData  ; User Id = sa; Password = CheekyKitty25!";
 
             services.AddDbContext<MetricContext>(options => options.UseSqlServer(connection));
 
