2c67e66f426cc9b74fe9a8bf9fb779b66a3cd59a
Jacob Kirsch <t-jakirs@microsoft.com>
2018-07-02 08:27:36 -0700

    added general controller

---
+++ src/DataTransfer/Controllers/GeneralController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+
+namespace PerfMonitor.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class General : ControllerBase
+    {
+        public MetricContext _MetricContext;
+
+        public General(MetricContext context)
+        {
+            _MetricContext = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        public async Task<IActionResult> CreateCPUDatapointFromJSON([FromBody]string j)
+        {
+            Metric_List met = new Metric_List();
+            met = JsonConvert.DeserializeObject<Metric_List>(j);
+            foreach (CPU_Usage point in met.cpu)
+            {
+                _MetricContext.CPU_Data.Add(point);
+            }
+            foreach(Mem_Usage point in met.mem)
+            {
+                _MetricContext.MEM_Data.Add(point);
+            }
+            await _MetricContext.SaveChangesAsync();
+            return CreatedAtAction("Data Created", new { obj = j }, null);
+        }
+
+        
+
+    }
+}
