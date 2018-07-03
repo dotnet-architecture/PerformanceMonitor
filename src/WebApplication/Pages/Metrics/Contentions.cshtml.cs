0c0fd6ea9883ac338941b305374ff706364e6fa7
mogarg18 <39780842+mogarg18@users.noreply.github.com>
2018-07-03 15:35:24 -0700

    updating Contentions page on web page

--- src/WebApplication/Pages/Metrics/Contentions.cshtml.cs
+++ src/WebApplication/Pages/Metrics/Contentions.cshtml.cs
@@ -1,17 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNet.SignalR.Client;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PerfMonitor;
+using WebApplication.Interfaces;
+using WebApplication.Services;
 
 namespace WebApplication.Pages.Metrics
 {
     public class ContentionsModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IMetricService<Contention> _contentionsMetricService = new MetricService<Contention>();
+        public List<Contention> contentions { get; set; } = new List<Contention>();
 
+        // Counter that detects when 5 seconds pass so HTTP get requests are sent every 5 seconds
+        // Will decide later on oldStamp, automatically set to a month previous to current time (gets data for a month range)
+        private DateTime oldStamp = DateTime.Today.AddMonths(-1).ToUniversalTime();
+        private DateTime newStamp = DateTime.Now.ToUniversalTime();
+
+        public async Task OnGet()
+        {
+            newStamp = DateTime.Now.ToUniversalTime();
+            List<Contention> addOn = await FetchDataService.getUpdatedData<Contention>(oldStamp, newStamp);
+
+            foreach (Contention c in addOn)
+            {
+                contentions.Add(c);
+            }
         }
     }
 }
