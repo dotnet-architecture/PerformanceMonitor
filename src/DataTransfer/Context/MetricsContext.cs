eabb7b2eab628925fcc2ec16aceeb37017480851
Jacob Kirsch <t-jakirs@microsoft.com>
2018-07-03 11:27:38 -0700

    added server functionality for all data types

--- src/DataTransfer/Context/MetricsContext.cs
+++ src/DataTransfer/Context/MetricsContext.cs
@@ -15,6 +15,9 @@ public class MetricContext : DbContext
     public DbSet<Mem_Usage> MEM_Data { get; set; }
     public DbSet<Exceptions> Exception_Data { get; set; }
     public DbSet<Http_Request> HTTP_Data { get; set; }
+    public DbSet<Contention> Contention_Data { get; set; }
+    public DbSet<Jit> Jit_Data { get; set; }
+    public DbSet<GC> GC_Data { get; set; }
 }
 
 class CPUContextEntityTypeConfiguration
@@ -49,6 +52,58 @@ class ExceptionContextEntityTypeConfiguration
     public void Configure(EntityTypeBuilder<Exceptions> builder)
     {
         builder.ToTable("Exception_Data");
+
+        builder.HasKey(ci => ci.timestamp);
+        builder.HasIndex(ci => ci.timestamp);
+    }
+
+}
+
+class HTTPContextEntityTypeConfiguration
+    : IEntityTypeConfiguration<Http_Request>
+{
+    public void Configure(EntityTypeBuilder<Http_Request> builder)
+    {
+        builder.ToTable("HTTP_Data");
+
+        builder.HasKey(ci => ci.timestamp);
+        builder.HasIndex(ci => ci.timestamp);
+    }
+
+}
+
+class ContentionContextEntityTypeConfiguration
+    : IEntityTypeConfiguration<Contention>
+{
+    public void Configure(EntityTypeBuilder<Contention> builder)
+    {
+        builder.ToTable("Contention_Data");
+
+        builder.HasKey(ci => ci.timestamp);
+        builder.HasIndex(ci => ci.timestamp);
+    }
+
+}
+
+class GCContextEntityTypeConfiguration
+    : IEntityTypeConfiguration<GC>
+{
+    public void Configure(EntityTypeBuilder<GC> builder)
+    {
+        builder.ToTable("GC_Data");
+
+        builder.HasKey(ci => ci.timestamp);
+        builder.HasIndex(ci => ci.timestamp);
+    }
+
+}
+
+class JitContextEntityTypeConfiguration
+    : IEntityTypeConfiguration<Jit>
+{
+    public void Configure(EntityTypeBuilder<Jit> builder)
+    {
+        builder.ToTable("JIT_Data");
 
         builder.HasKey(ci => ci.timestamp);
         builder.HasIndex(ci => ci.timestamp);
--- src/DataTransfer/SQL_Commands/CREATE_BLANK_EXCEPTION_TABLE.sql
+++ src/DataTransfer/SQL_Commands/CREATE_BLANK_EXCEPTION_TABLE.sql
@@ -1,5 +1,5 @@
-DROP TABLE Exception_Data
-CREATE TABLE Exception_data (
+DROP TABLE IF EXISTS Exception_Data
+CREATE TABLE Exception_Data (
 "type" VARCHAR(MAX), 
 "timestamp" DATETIME PRIMARY KEY 
 )
