3ac2d60ae82492f015e8a4ea07167e6a7984aef1
McCoy Patino Middaugh <t-mcpati@microsoft.com>
2018-07-09 10:22:34 -0700

    Added GC and Exception tests

--- test/Measurement/Program.cs
+++ test/Measurement/Program.cs
@@ -9,25 +9,8 @@ namespace MonitorTest
         {
             Monitor monitor = new Monitor();
             monitor.Record();
-            //System.Threading.Thread t = new System.Threading.Thread(new System.Threading.ThreadStart(ThreadProc));
-            //t.Start();
-            DateTime timer = DateTime.Now;
-            while (true)
-            {
-                if (DateTime.Now.Subtract(timer).TotalSeconds >= 1)
-                {
-                    System.GC.Collect();
-                    System.GC.WaitForPendingFinalizers();
-                    timer = DateTime.Now;
-                }
-            }
-        }
-        static void ThreadProc()
-        {
-            while (true)
-            {
-                continue;
-            }
+            //GCTest.Test();
+            //ExceptionTest.Test();
         }
     }
 }
