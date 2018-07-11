766a44018e5dec1b6e275b6f0f4fe03a47e96168
McCoy Patino <39780829+mccoyp@users.noreply.github.com>
2018-07-11 14:57:20 -0700

    Added sampling/transmission rates to Session class (#81)

--- src/Shared/Session.cs
+++ src/Shared/Session.cs
@@ -6,6 +6,8 @@ namespace DataTransfer
     {
         public String app { get; set; }
         public String process { get; set; }
+        public int sampleRate { get; set; }
+        public int sendRate { get; set; }
         public int processorCount { get; set; }
         public String os { get; set; }
     }
