a8982d6074a4a7574a5261ac140aa4f425efb7f3
Jacob Kirsch <39781049+jakirsatms@users.noreply.github.com>
2018-07-16 13:12:50 -0700

    Basic test (#84)
    * added server functionality for all data types
    
    * fixed up SQL commands
    
    * added a basic test project to see test functionality
    
    * fixed SQL stuff
    
    * Added GC and Exception tests (#75)
    
    * added files for testing if server is up
    
    * added basic test to see if server is up
    
    * added basic test to see if server is up

--- test/TestDataTransfer/UnitTest1.cs
+++ test/TestDataTransfer/UnitTest1.cs
@@ -7,11 +7,13 @@ namespace UnitTestProject1
     public class TestTransfer
     {
         public TestTransfer() { }
+
         [Fact]
         public void TestSQLConnection()
         {
             bool isOpen = false;
             var connection = "Server = 10.0.75.1,1433; Initial Catalog = PerformanceData  ; User Id = sa; Password = Abc12345";
+
             using (var serCon = new SqlConnection(connection))
             {
                 try
@@ -25,20 +27,6 @@ namespace UnitTestProject1
                 }
             }
             Assert.True(isOpen);
-
-            var connection = "Server = 10.0.75.1,1433; Initial Catalog = PerformanceData  ; User Id = sa; Password = JBKmichigan20";
-            using (var serCon = new SqlConnection(connection))
-            {
-                try
-                {
-                    serCon.Open();
-                    return true;
-                }
-                catch
-                {
-                    return false;
-                }
-            }
         }
     }
 }
