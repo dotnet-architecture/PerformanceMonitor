e05546353453ba7df7371fa832f5393fbecf0ab4
mogarg18 <39780842+mogarg18@users.noreply.github.com>
2018-07-17 15:17:01 -0700

    merge conflicts

--- src/WebApplication/Startup.cs
+++ src/WebApplication/Startup.cs
@@ -29,8 +29,8 @@ namespace WebApplication
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
             services.AddSignalR();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- test/Measurement/Program.cs
+++ test/Measurement/Program.cs
@@ -9,12 +9,8 @@ namespace MonitorTest
         {
             Monitor monitor = new Monitor("App");
             monitor.Record();
-<<<<<<< HEAD
 
             CPUMemTest.Test();
-=======
-            //CPUMemTest.Test();
->>>>>>> upstream/master
             //GCTest.Test();
             //ExceptionTest.Test();
             ContentionTest.Test();
