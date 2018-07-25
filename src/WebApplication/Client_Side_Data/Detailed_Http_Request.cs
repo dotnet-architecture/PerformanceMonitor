95784038d2cf5dfb2d6f1f571c8c261cccfd5f4a
mogarg18 <39780842+mogarg18@users.noreply.github.com>
2018-07-24 17:33:40 -0700

    created http request class for client side and method to compute duration of http request

---
+++ src/WebApplication/Client_Side_Data/Detailed_Http_Request.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.ClientSideData
+{
+    public class Detailed_Http_Request
+    {
+        public string method { get; set; }
+        public string path { get; set; }
+        public Guid id { get; set; }
+        public DateTime startTimestamp { get; set; }
+        public DateTime endTimestamp { get; set; }
+        public long duration { get; set; } //measured in ms
+
+        public Boolean updateDuration(){
+            if (startTimestamp != null && endTimestamp != null)
+            {
+                return false;
+            }
+
+            TimeSpan span = endTimestamp.Subtract(startTimestamp);
+            duration = span.Milliseconds;
+            return true;
+        }
+}
+}
