a53ad3987df51fb8ed269eebfccd34b4a32bcf03
Jacob Kirsch <39781049+jakirsatms@users.noreply.github.com>
2018-07-27 12:08:36 -0700

    updated general class (#123)

--- src/DataTransfer/Controllers/GeneralController.cs
+++ src/DataTransfer/Controllers/GeneralController.cs
@@ -16,6 +16,8 @@ namespace DataTransfer.Controllers
     {
         public PerformanceDataContext _MetricContext;
 
+        private static object lock_obj = new object();
+
         public General(PerformanceDataContext context)
         {
             _MetricContext = context ?? throw new ArgumentNullException(nameof(context));
@@ -33,43 +35,55 @@ namespace DataTransfer.Controllers
             Session info = await _MetricContext.Session.SingleOrDefaultAsync(s => (s.application == met.session.application && s.process == met.session.process));
             if (null == info)
             {
-                _MetricContext.Session.Add(met.session);
-                await _MetricContext.SaveChangesAsync();
+                lock (lock_obj)
+                {
+                    Task<Session> obj =  _MetricContext.Session.SingleOrDefaultAsync(s => (s.application == met.session.application && s.process == met.session.process));
+                    obj.Wait();
+                    info = obj.Result;
+                    if (null == info)
+                    {
+                        _MetricContext.Session.Add(met.session);
+                        _MetricContext.SaveChangesAsync();
+                        info = met.session;
+                    }
+                }
+              
             }
+
             //Adds datapoints to respective tables
             foreach (CPU_Usage point in met.cpu)
             {
-                point.AppId = met.session.Id;
+                point.AppId = info.Id;
                 _MetricContext.CPU_Usage.Add(point);
             }
             foreach (Mem_Usage point in met.mem)
             {
-                point.AppId = met.session.Id;
+                point.AppId = info.Id;
                 _MetricContext.MemData.Add(point);
             }
             foreach (Exceptions point in met.exceptions)
             {
-                point.AppId = met.session.Id;
+                point.AppId = info.Id;
                 _MetricContext.Exceptions.Add(point);
             }
             foreach (Http_Request point in met.requests)
             {
-                point.AppId = met.session.Id;
+                point.AppId = info.Id;
                 _MetricContext.Http_Request.Add(point);
             }
             foreach (Jit point in met.jit)
             {
-                point.AppId = met.session.Id;
+                point.AppId = info.Id;
                 _MetricContext.Jit.Add(point);
             }
             foreach (Contention point in met.contentions)
             {
-                point.AppId = met.session.Id;
+                point.AppId = info.Id;
                 _MetricContext.Contention.Add(point);
             }
             foreach (GC point in met.gc)
             {
-                point.AppId = met.session.Id;
+                point.AppId = info.Id;
                 _MetricContext.GC.Add(point);
             }
             await _MetricContext.SaveChangesAsync();
