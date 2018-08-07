db593d0c3c74d34db5b9a1558a2ad719b3824d1f
McCoy Patino <39780829+mccoyp@users.noreply.github.com>
2018-08-07 11:58:41 -0700

    Updated readme and added console app (#152)

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DataTransfer;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Monitor monitor = new Monitor("Process1", "ConsoleApp");
+            monitor.Record();
+            Console.WriteLine("Hello World!");
+            while (true) ;
+        }
+    }
+}
